2a7b4ce7b84e8403640c47d073e158b92e1856c4
Vladlanov <bulanov1192@gmail.com>
2018-04-22 02:59:13 +0500

    Projects page almost ready

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
 /**
  * Created by HYPERPC on 22.04.2018.
  */
+
+// POPUP toggler
+$('.project-add_btn').click(function() {
+   $('.overlay').toggleClass('hidden');
+});
+
+$('.popup_close').click(function() {
+    $('.overlay').addClass('hidden');
+});
+
+$('.overlay').click(function() {
+    $(this).addClass('hidden');
+});
